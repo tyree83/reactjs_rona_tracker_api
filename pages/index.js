1e584d219427c037b8a112521a459cc4fbcba3a0
Tyree Christensen <tyree83@gmail.com>
2021-02-17 14:25:36 -0500

    added styling

---
+++ pages/index.js
@@ -0,0 +1,32 @@
+  
+import { createGlobalStyle } from 'styled-components';
+import Stats from '../components/Stats';
+import CountrySelector from '../components/CountrySelector';
+import Header from '../components/Header';
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    /* border: 5px solid greenyellow; */
+    background-color: #EFEFEE;
+    display: flex;
+    flex: 1;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+      h1 {color: #EFEFEE;}
+      h2 {color: #0FADB2}
+      h4 {color: #0FADB2}
+  }
+`;
+
+export default function IndexPage() {
+  return (
+    <div>
+      <GlobalStyle />
+      <h2>Global Coronavirus Stats</h2>
+      <Stats url="https://covid19.mathdro.id/api"></Stats>
+      <CountrySelector></CountrySelector>
+    </div>
+  );
+}
